0652667a530228eb3fc1b3037aa640503a93f8c2
zahid-01 <erzahidkhan101@gmail.com>
2025-09-29 18:59:05 +0530

    reading the datasets

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -7,8 +7,10 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
-# X = dataset.iloc[:, :-1].values
-# y = dataset.iloc[:, -1].values
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1 ].values
+
+print(Y)
 
 # # Splitting the dataset into the Training set and Test set
 # from sklearn.model_selection import train_test_split
